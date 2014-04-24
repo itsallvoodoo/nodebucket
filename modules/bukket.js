28032c668d51886be62d45efb9ca8caece582daf
itsallvoodoo <chaddhobbs@gmail.com>
2014-04-24 17:00:04 -0400

    Stripping it down for modularity

---
+++ modules/bukket.js
@@ -0,0 +1,23 @@
+/*
+* name:     bukket.js
+* author:   Chad Hobbs
+* created:  140327
+*
+* description: This is the bukket module for nodebot, stylized like xkcd-bucket
+*/
+
+// ----------------------------------------------------------COPYPASTA OF ALL------------------------------------------
+
+var bukket = {
+	hello: " is what was sent.",
+
+	concat: function(a,b) {
+		return a + b;
+	},
+
+	say: function(text) {
+		return text;
+	}
+}
+
+exports.bukket = bukket;	
